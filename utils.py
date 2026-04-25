9dd8bf47879f945d780fc00e45b52a3d05887c61
Areen <1coding0areen.js@gmail.com>
2026-04-25 21:36:30 +0530

    utils: add format_results function

--- utils.py
+++ utils.py
@@ -2,3 +2,8 @@ def clean_text(text):
     """function which converts text to lowercase and return it"""
     text = text.strip().lower()
     return text
+
+
+def format_results(word, count):
+    """function which format the results in word : count format"""
+    return f"{word}: {count} times"
